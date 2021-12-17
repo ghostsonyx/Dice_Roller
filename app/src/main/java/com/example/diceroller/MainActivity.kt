2dbda2b32851bcb6394c3a997be4e8bc985d5bb6
ghostsonyx <66806782+ghostsonyx@users.noreply.github.com>
2021-12-16 19:58:48 -0600

    Add dice images using a when condition when rolling

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 /**
@@ -15,20 +16,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener {
+            val rollResult = rollDice()
+            showRollResultOutput(rollResult)
+        }
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice() {
+    private fun rollDice(): Int {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        return diceRoll;
+    }
+
+    private fun showRollResultOutput(rollResult: Int) {
+        val drawableResource = when(rollResult) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> {R.drawable.ic_launcher_background}
+        }
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = rollResult.toString()
     }
 }
 
